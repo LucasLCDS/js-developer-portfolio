f56100b49ec88870e58ef79d2314f7a8f2dbb7f2
Lucas Correia da Silva <108384166+LucasLCDS@users.noreply.github.com>
2023-03-22 08:43:02 -0300

    Atualização

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/LucasLCDS/js-developer-portifolio/main/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/LucasLCDS/js-developer-portfolio/main/data/profile.json'
     const fetching = fetch(url);
     return await (await fetching).json();
 }
